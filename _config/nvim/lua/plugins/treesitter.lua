ee01bcb301cab3df1dc438cdc487ebd295382b7e
Carl You <4531192+carlyou@users.noreply.github.com>
2026-01-11 22:23:21 -0800

    nvim treesitter context

--- _config/nvim/lua/plugins/treesitter.lua
+++ _config/nvim/lua/plugins/treesitter.lua
@@ -102,4 +102,8 @@ return {
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {},
+  },
 }
